2cff4e418c514edc9566c17d753dc3fd7ac7efd3
Alexander Smalkov <alexander@smalkov.net>
2025-05-15 22:26:48 -0700

    Delete tests/restartScada.sh

--- tests/restartScada.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-declare -A a=(["/opt/mplc4/"]="/etc/init.d/mplc4 restart" ["/opt/SibMir_SCADA/"]="/etc/init.d/SibMir_SCADA restart")
-
-for b in "${!a[@]}"; do
-    if [ -d "$b" ]; then
-        sudo su -c "${a[$b]}"
-    fi
-done
