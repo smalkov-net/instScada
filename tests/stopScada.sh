4a61d77d905ce0f37cad879d0bf063d4087c1859
Alexander Smalkov <alexander@smalkov.net>
2025-05-15 22:27:06 -0700

    Delete tests/stopScada.sh

--- tests/stopScada.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-declare -A a=(["/opt/mplc4/"]="/etc/init.d/mplc4 stop" ["/opt/SibMir_SCADA/"]="/etc/init.d/SibMir_SCADA stop")
-
-for b in "${!a[@]}"; do
-    if [ -d "$b" ]; then
-        sudo su -c "${a[$b]}"
-    fi
-done
